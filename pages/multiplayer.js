6a9c893435aef3b454e6a4415cb27815dbc94f5a
Andrei Antoni Bonatto <andrei.bonatto@brsulnetad.local>
2025-05-30 18:06:30 -0300

    vevercel

--- pages/multiplayer.js
+++ pages/multiplayer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Head from 'next/head';
 import { useLanguage } from '../contexts/LanguageContext';
 import { MultiplayerProvider, useMultiplayerContext } from '../contexts/MultiplayerContext';
